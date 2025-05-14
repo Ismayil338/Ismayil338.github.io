10a59c09f1c640696687e2107519f46d37e4957b
Ismayil <150678829+Ismayil338@users.noreply.github.com>
2025-05-14 21:43:09 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -42,3 +42,31 @@ function setDetailedView() {
         grid.classList.add('list-view');
     });
 }
+
+// Store data in JSON file
+const currentPage = window.location.pathname
+  .split("/")
+  .pop()
+  .replace(".html", "");
+
+fetch('../../js/data.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('software-list');
+    if (!container) return; // Avoids errors if container is missing on some pages
+
+    data
+      .filter(item => item.pages.includes(currentPage))
+      .forEach(item => {
+        container.innerHTML += `
+          <div class="software-item">
+            <a href="${item.link}">
+              <img src="${item.image}">
+              <p>${item.title}</p>
+              <p class="title-ru">${item['title-ru']}</p>
+            </a>
+          </div>
+        `;
+      });
+  })
+  .catch(error => console.error("Error loading data:", error));
